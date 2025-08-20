52c2911c261da7b2b8169172c9fe18b2a7871edf
Mokom <samamokom50@gmail.com>
2025-08-20 12:50:33 +0100

    CORS Adjustments

--- Backend/server.js
+++ Backend/server.js
@@ -30,6 +30,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "https://x-clusive.onrender.com",
+      "https://x-clussive-shop.onrender.com",
     ],
     credentials: true,
     optionsSuccessStatus: 200,
